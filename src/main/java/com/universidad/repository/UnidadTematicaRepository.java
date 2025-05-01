87484a546d89c017e6ed415a33e5c989346671a9
ronaldmendzas <ronaldmencas@gmail.com>
2025-04-30 23:34:32 -0400

    Eliminación total de contenido anterior y subida de proyecto final limpio

--- src/main/java/com/universidad/repository/UnidadTematicaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.universidad.repository;
-
-import com.universidad.model.UnidadTematica;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface UnidadTematicaRepository extends JpaRepository<UnidadTematica, Long> {
-    List<UnidadTematica> findByMateriaId(Long materiaId);
-}
